c01c7c7c600f64ed80c138c408a9b6164b52abc8
Kwon-soonchan <clover9593@naver.com>
2026-04-05 23:36:38 +0900

    chore: Spring Boot 초기 프로젝트 세팅

---
+++ src/main/java/com/example/dormlaundryalarm/DormLaundryAlarmApplication.java
@@ -0,0 +1,13 @@
+package com.example.dormlaundryalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormLaundryAlarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormLaundryAlarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dormlaundryalarm/DormLaundryAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dormlaundryalarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormLaundryAlarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
